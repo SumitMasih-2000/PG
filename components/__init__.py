7058752549b2f35825fb13ca17885868a0aea3a3
SumitMasih-2000 <sumit.masih@msfa.christuniversity.in>
2026-07-04 16:07:26 +0530

    Initialize components package with imports

---
+++ components/__init__.py
@@ -0,0 +1,5 @@
+# Initialize components package
+from .navbar import render_navbar
+from .footer import render_footer
+from .cards import render_property_card, render_metric_card
+from .forms import render_login_form, render_registration_form
